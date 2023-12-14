4277e3a8be4b6061178725ba6833555f047ebd75
Martin Unger <126020447+MartinUnger98@users.noreply.github.com>
2023-12-14 09:52:24 +0000

    mainpage + footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    MainPageComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
